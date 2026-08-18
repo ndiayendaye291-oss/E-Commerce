6c70c7a6d11268c2450aac96205a0e88035dca03
Becaye seydi <becayeseydi265@email.com>
2026-08-18 23:41:58 +0000

    Configure production deployment settings (requirements.txt, Procfile, WhiteNoise)

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'corsheaders.middleware.CorsMiddleware',
